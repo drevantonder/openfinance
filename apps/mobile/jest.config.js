7d909205c752fba19858d0a721ed016ca8ff07c6
Andre van Tonder <andre@drevan.me>
2026-01-21 07:15:04 +1000

    fix: ci and test configs

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
   testMatch: [
